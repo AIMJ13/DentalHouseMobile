73cec5d607df93f54b2b6df3cdc86f09fd2631a7
Axel200514 <marcuslvl13@gmail.com>
2026-09-22 14:57:12 -0600

    limpia import sin uso en main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'Screen/Home/home_screen.dart';
 import 'Screen/Login/login_screen.dart';
-import 'Screen/DoctorHome/doctor_home_screen.dart';
 import 'routes.dart';
 
 void main() async {
